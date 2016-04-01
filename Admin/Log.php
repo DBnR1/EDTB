10b5b550a8d0cf1fa584da7871c9af54cf184a24
Mauri Kujala <contact@edtb.xyz>
2016-04-01 17:55:11 +0300

    Small bug fix

--- Admin/Log.php
+++ Admin/Log.php
@@ -53,7 +53,9 @@ $header->display_header();
         </h2>
         <hr>
         <?php
-        // read logfile
+        /**
+         * read logfile
+         */
         $logfile = $_SERVER["DOCUMENT_ROOT"] . "/edtb_log.txt";
         $lines = file($logfile);
         ?>
@@ -68,76 +70,76 @@ $header->display_header();
                 </tr>
             </thead>
             <tbody>
-            <?php
-            /**
-             * reverse the array and output data
-             */
-            if (!empty($lines)) {
-                foreach (array_reverse($lines) as $line_num => $line) {
-                    /**
-                     * only show first 600 lines
-                     */
-                    if ($line_num <= 599) {
-                        // Regular Expression filter for links
-                        $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-
-                        /**
-                         * split data and define variables
-                         */
-                        $data = explode("]", $line);
-                        $time = str_replace("[", "", $data[0]);
-
-                        $parts = explode(" on line ", $data[1]);
-                        $error_line = $parts[1];
-                        $error_line = empty($error_line) ? "n/a" : $error_line;
-
-                        $file = str_replace("[", "", $parts[0]);
-                        $file = str_replace($settings["install_path"] . "\\EDTB\\", "", $file);
-                        $file = empty($file) ? "n/a" : $file;
-
-                        $error = array_slice($data, 2);
-                        $error = implode("", $error);
-
-                        if (preg_match($reg_exUrl, $error, $url)) {
-                            $error = preg_replace($reg_exUrl, '<a href="' . $url[0] . '" target="_blank">' . $url[0] . '</a>', $error);
-                        } else {
-                            $error = strip_tags($error);
-                        }
-
-                        $tdclass = $line_num % 2 ? "dark" : "light";
-                        ?>
-                        <tr>
-                            <td class="<?php echo $tdclass?>" style="padding:10px;width:1%;text-align:center">
-                                <a class="copy" href="javascript:void(0);" title="Copy to clipboard" data-clipboard-text="<?php echo $line?>">
-                                    <img class="icon" src="/style/img/clipboard.png" alt="Copy" style="margin-left:5px" />
-                                </a>
-                            </td>
-                            <td class="<?php echo $tdclass?>" style="width:1%;text-align:center">
-                                <?php echo $time?>
-                            </td>
-                            <td class="<?php echo $tdclass?>">
-                                <?php echo $file?>
-                            </td>
-                            <td class="<?php echo $tdclass?>">
-                                <?php echo $error_line?>
-                            </td>
-                            <td class="<?php echo $tdclass?>">
-                                <?php echo $error?>
-                            </td>
-                        </tr>
-                        <?php
-                    }
-                }
-            } else {
-                ?>
-                <tr>
-                    <td class="dark" colspan="5">
-                        The log file is empty.
-                    </td>
-                </tr>
                 <?php
-            }
-            ?>
+                /**
+                 * reverse the array and output data
+                 */
+                if (!empty($lines)) {
+                    foreach (array_reverse($lines) as $line_num => $line) {
+                        /**
+                         * only show first 600 lines
+                         */
+                        if ($line_num <= 599) {
+                            // Regular Expression filter for links
+                            $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+
+                            /**
+                             * split data and define variables
+                             */
+                            $data = explode("]", $line);
+                            $time = str_replace("[", "", $data[0]);
+
+                            $parts = explode(" on line ", $data[1]);
+                            $error_line = $parts[1];
+                            $error_line = empty($error_line) ? "n/a" : $error_line;
+
+                            $file = str_replace("[", "", $parts[0]);
+                            $file = str_replace($settings["install_path"] . "\\EDTB\\", "", $file);
+                            $file = empty($file) ? "n/a" : $file;
+
+                            $error = array_slice($data, 2);
+                            $error = implode("", $error);
+
+                            if (preg_match($reg_exUrl, $error, $url)) {
+                                $error = preg_replace($reg_exUrl, '<a href="' . $url[0] . '" target="_blank">' . $url[0] . '</a>', $error);
+                            } else {
+                                $error = strip_tags($error);
+                            }
+
+                            $tdclass = $line_num % 2 ? "dark" : "light";
+                            ?>
+                            <tr>
+                                <td class="<?php echo $tdclass?>" style="padding:10px;width:1%;text-align:center">
+                                    <a class="copy" href="javascript:void(0);" title="Copy to clipboard" data-clipboard-text="<?php echo $line?>">
+                                        <img class="icon" src="/style/img/clipboard.png" alt="Copy" style="margin-left:5px" />
+                                    </a>
+                                </td>
+                                <td class="<?php echo $tdclass?>" style="width:1%;text-align:center">
+                                    <?php echo $time?>
+                                </td>
+                                <td class="<?php echo $tdclass?>">
+                                    <?php echo $file?>
+                                </td>
+                                <td class="<?php echo $tdclass?>">
+                                    <?php echo $error_line?>
+                                </td>
+                                <td class="<?php echo $tdclass?>">
+                                    <?php echo strip_tags($error, "<a>")?>
+                                </td>
+                            </tr>
+                            <?php
+                        }
+                    }
+                } else {
+                    ?>
+                    <tr>
+                        <td class="dark" colspan="5">
+                            The log file is empty.
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
             </tbody>
         </table>
     </div>
